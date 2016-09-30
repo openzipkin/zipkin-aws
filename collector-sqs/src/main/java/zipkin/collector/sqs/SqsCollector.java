220e3a084e92a9f37dddc517e1534a7a73d190d5
Brian Devins <brian.devins@dealer.com>
2016-09-29 22:27:04 -0400

    Start of sqs collector

---
+++ collector-sqs/src/main/java/zipkin/collector/sqs/SqsCollector.java
@@ -0,0 +1,92 @@
+package zipkin.collector.sqs;
+
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.services.sqs.AmazonSQSClient;
+import zipkin.collector.Collector;
+import zipkin.collector.CollectorComponent;
+import zipkin.collector.CollectorMetrics;
+import zipkin.collector.CollectorSampler;
+import zipkin.internal.LazyCloseable;
+import zipkin.storage.StorageComponent;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+public class SqsCollector implements CollectorComponent, Closeable {
+
+    private Collector collector;
+    private AWSCredentialsProvider credentialsProvider;
+    private String sqsQueueUrl;
+
+    private AmazonSQSClient client;
+
+    public SqsCollector(Builder builder) {
+        collector = builder.delegate.build();
+        credentialsProvider = builder.credentialsProvider;
+        sqsQueueUrl = builder.sqsQueueUrl;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static final class Builder implements CollectorComponent.Builder {
+        Collector.Builder delegate = Collector.builder(SqsCollector.class);
+
+        String sqsQueueUrl;
+        AWSCredentialsProvider credentialsProvider = new DefaultAWSCredentialsProviderChain();
+
+        @Override
+        public CollectorComponent.Builder storage(StorageComponent storage) {
+            delegate.storage(storage);
+            return this;
+        }
+
+        @Override
+        public CollectorComponent.Builder metrics(CollectorMetrics metrics) {
+            delegate.metrics(metrics);
+            return this;
+        }
+
+        @Override
+        public CollectorComponent.Builder sampler(CollectorSampler sampler) {
+            delegate.sampler(sampler);
+            return this;
+        }
+
+        public Builder sqsQueueUrl(String sqsQueueUrl) {
+            this.sqsQueueUrl = sqsQueueUrl;
+            return this;
+        }
+
+        public Builder awsCredentialsProvider(AWSCredentialsProvider awsCredentialsProvider) {
+            this.credentialsProvider = awsCredentialsProvider;
+            return this;
+        }
+
+        @Override
+        public SqsCollector build() {
+            return new SqsCollector(this);
+        }
+
+        Builder() {
+        }
+    }
+
+    @Override
+    public CollectorComponent start() {
+        client = new AmazonSQSClient(credentialsProvider);
+        return this;
+    }
+
+    @Override
+    public CheckResult check() {
+        return null;
+    }
+
+    @Override
+    public void close() throws IOException {
+        client.shutdown();
+    }
+}
