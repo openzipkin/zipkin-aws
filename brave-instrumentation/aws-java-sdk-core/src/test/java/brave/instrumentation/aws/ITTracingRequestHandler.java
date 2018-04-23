5d92009a32e2811a0030381886504bba4dedd3cd
Brian Devins <brian.devins@coxautoinc.com>
2018-04-23 13:52:49 -0400

    Implement a couple more integration tests

--- brave-instrumentation/aws-java-sdk-core/src/test/java/brave/instrumentation/aws/ITTracingRequestHandler.java
+++ brave-instrumentation/aws-java-sdk-core/src/test/java/brave/instrumentation/aws/ITTracingRequestHandler.java
@@ -13,12 +13,19 @@
  */
 package brave.instrumentation.aws;
 
+import brave.SpanCustomizer;
+import brave.http.HttpAdapter;
+import brave.http.HttpClientParser;
 import brave.test.http.ITHttpAsyncClient;
 import com.amazonaws.ClientConfiguration;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsyncClientBuilder;
 import java.util.Collections;
+import okhttp3.mockwebserver.MockResponse;
+import zipkin2.Span;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ITTracingRequestHandler extends ITHttpAsyncClient<AmazonDynamoDB> {
   @Override protected void getAsync(AmazonDynamoDB dynamoDB, String s) throws Exception {
@@ -31,7 +38,7 @@ public class ITTracingRequestHandler extends ITHttpAsyncClient<AmazonDynamoDB> {
     clientConfiguration.setRequestTimeout(1000);
 
     return AmazonDynamoDBAsyncClientBuilder.standard()
-        .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://localhost:" + i, "us-east-1"))
+        .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://127.0.0.1:" + i, "us-east-1"))
         .withRequestHandlers(TracingRequestHandler.create(httpTracing))
         .withClientConfiguration(clientConfiguration)
         .build();
@@ -42,13 +49,69 @@ public class ITTracingRequestHandler extends ITHttpAsyncClient<AmazonDynamoDB> {
   }
 
   @Override protected void get(AmazonDynamoDB dynamoDB, String s) throws Exception {
-    dynamoDB.getItem("s", Collections.EMPTY_MAP);
+    dynamoDB.getItem(s, Collections.EMPTY_MAP);
   }
 
   @Override protected void post(AmazonDynamoDB dynamoDB, String s, String s1) throws Exception {
-    dynamoDB.getItem("s", Collections.EMPTY_MAP);
+    dynamoDB.getItem(s, Collections.EMPTY_MAP);
   }
 
+  /*
+   * Tests overridden due to RPC nature of AWS API
+   */
+
+  /** Span name doesn't conform to expectation */
+  @Override public void supportsPortableCustomization() throws Exception {
+    String uri = "";
+    close();
+    httpTracing = httpTracing.toBuilder()
+        .clientParser(new HttpClientParser() {
+          @Override
+          public <Req> void request(brave.http.HttpAdapter<Req, ?> adapter, Req req,
+              SpanCustomizer customizer) {
+            customizer.tag("http.url", adapter.url(req)); // just the path is logged by default
+            customizer.tag("request_customizer.is_span", (customizer instanceof brave.Span) + "");
+          }
+
+          @Override
+          public <Resp> void response(HttpAdapter<?, Resp> adapter, Resp res, Throwable error,
+              SpanCustomizer customizer) {
+            super.response(adapter, res, error, customizer);
+            customizer.tag("response_customizer.is_span", (customizer instanceof brave.Span) + "");
+          }
+        })
+        .build().clientOf("remote-service");
+
+    client = newClient(server.getPort());
+    server.enqueue(new MockResponse());
+    get(client, uri);
+
+    Span span = takeSpan();
+
+    assertThat(span.remoteServiceName())
+        .isEqualTo("amazondynamodbv2");
+
+    assertThat(span.tags())
+        .containsEntry("http.url", url(uri))
+        .containsEntry("request_customizer.is_span", "false")
+        .containsEntry("response_customizer.is_span", "false");
+  }
+
+  /** Body's inherently have a structure */
+  @Override public void post() throws Exception {
+    String path = "table";
+    String body = "{\"TableName\":\"table\",\"Key\":{}}";
+    server.enqueue(new MockResponse());
+
+    post(client, path, body);
+
+    assertThat(server.takeRequest().getBody().readUtf8())
+        .isEqualTo(body);
+
+    Span span = takeSpan();
+    assertThat(span.name())
+        .isEqualTo("post");
+  }
 
   /*
    * Tests that don't work
@@ -57,22 +120,16 @@ public class ITTracingRequestHandler extends ITHttpAsyncClient<AmazonDynamoDB> {
   /** AWS doesn't use redirect */
   @Override public void redirect() {}
 
-  /** Span name doesn't conform to expectation */
-  @Override public void supportsPortableCustomization() {}
-
   /** Paths don't conform to expectation, always / */
   @Override public void customSampler() {}
 
-  /** Body's inherently have a structure */
-  @Override public void post() {}
-
-  /** Unable to parse remote endpoint */
+  /** Unable to parse remote endpoint IP, would require DNS lookup */
   @Override public void reportsServerAddress() {}
 
   /** Path is always empty */
   @Override public void httpPathTagExcludesQueryParams() {}
 
-  /** Error has exception instead of code */
+  /** Error has exception instead of status code */
   @Override public void addsStatusCodeWhenNotOk() {}
 
   /** All http methods are POST */
